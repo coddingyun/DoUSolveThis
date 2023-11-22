85261544a3e2f2fcafdf358052334491736b4926
coddingyun <igy2840@gmail.com>
2023-11-22 22:07:47 +0900

    style: landing page style 변경

--- src/layout/CommonLayout.jsx
+++ src/layout/CommonLayout.jsx
@@ -4,7 +4,7 @@ const CommonLayout = ({ children, title, subTitle }) => {
   return (
     <div className="w-full h-screen grid place-items-center">
       <div className="w-2/5 h-full py-10 px-4 grid grid-rows-[auto,1fr] gap-4">
-        <div className="grid grid-rows-[auto,auto] gap-2">
+        <div className="grid grid-rows-[auto,auto]">
           <div className="text-base font-normal text-gray-600">
             {subTitle || '이 문제 푸셨나요?'}
           </div>
--- src/page/Landing.jsx
+++ src/page/Landing.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import CommonLayout from '../layout/CommonLayout';
 import { ReactComponent as Search } from '../asset/search.svg';
 import { ReactComponent as Study } from '../asset/study.svg';
+import { ReactComponent as Export } from '../asset/export.svg';
 
-const LandingButton = ({ img, title, clickEvent }) => {
-  let colorNum = 1;
-  if (title === '스터디 만들기') {
-    colorNum = 2;
+const LandingButton = ({ img, title, clickEvent, bgColor }) => {
+  let customColor;
+
+  if (bgColor === 'purple') {
+    customColor =
+      'bg-purple-500 hover:bg-purple-400 focus-visible:outline-purple-500';
+  } else if (bgColor === 'indigo') {
+    customColor =
+      'bg-indigo-500 hover:bg-indigo-400 focus-visible:outline-indigo-500';
+  } else {
+    customColor =
+      'bg-blue-500 hover:bg-blue-400 focus-visible:outline-blue-500';
   }
-  const buttonClasses = `w-full h-40 grid place-items-center rounded-md ${
-    colorNum === 1
-      ? 'bg-blue-600 hover:bg-blue-500 focus-visible:outline-blue-600'
-      : 'bg-indigo-600 hover:bg-indigo-500 focus-visible:outline-indigo-600'
-  } px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2`;
+  const buttonClasses = `w-full h-10 flex items-center justify-center gap-2 rounded-full px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ${customColor}`;
+
   return (
     <span className="sm:ml-3">
       <button type="button" className={buttonClasses} onClick={clickEvent}>
         {img}
-        <div className="text-3xl font-bold">{title}</div>
+        <div className="text-xl font-bold">{title}</div>
       </button>
     </span>
   );
@@ -27,28 +32,46 @@ const LandingButton = ({ img, title, clickEvent }) => {
 const Landing = () => {
   const navigate = useNavigate();
   return (
-    <CommonLayout
-      subTitle="코딩 테스트 스터디 관리 서비스"
-      title="이 문제 푸셨나요?"
-    >
-      <div className="flex flex-col justify-between">
-        <div className="w-full text-base font-semibold text-gray-600">
-          스터디원이 푼 문제인지 쉽게 확인해보세요. <br />
-          함께 풀고 싶은 문제를 제안해보세요.
+    // <CommonLayout
+    //   subTitle="코딩 테스트 스터디 관리 서비스"
+    //   title="이 문제 푸셨나요?"
+    // >
+    <div className="w-full h-screen grid place-items-center py-20">
+      <div className="h-full flex flex-col justify-evenly">
+        <div>
+          <div className="text-lg font-semibold text-gray-600 text-center">
+            코딩 테스트 스터디 관리 서비스
+          </div>
+          <div className="text-5xl font-extrabold text-gray-800 text-center">
+            이 문제 푸셨나요?
+          </div>
+          <div className="w-full text-base font-semibold text-gray-600 text-center mt-5">
+            스터디원이 푼 문제인지 쉽게 확인해보세요. <br />
+            함께 풀고 싶은 문제를 제안해보세요.
+          </div>
         </div>
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-2">
           <LandingButton
-            img={<Search stroke-width="3" class="w-16 h-16" />}
-            title="스터디 찾기"
+            img={<Export strokeWidth="3" className="w-6 h-6" />}
+            title="최근 스터디 바로가기"
             clickEvent={() => navigate('/search')}
+            bgColor="blue"
           />
           <LandingButton
-            img={<Study stroke-width="3" class="w-16 h-16" />}
+            img={<Search strokeWidth="3" className="w-6 h-6" />}
+            title="스터디 찾기"
+            clickEvent={() => navigate('/search')}
+            bgColor="indigo"
+          />
+          <LandingButton
+            img={<Study strokeWidth="3" className="w-6 h-6" />}
             title="스터디 만들기"
+            clickEvent={() => navigate('/')}
+            bgColor="purple"
           />
         </div>
       </div>
-    </CommonLayout>
+    </div>
   );
 };
 
