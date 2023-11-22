a3057337b77539b9ce84b93f67b51c30ff9b3596
coddingyun <igy2840@gmail.com>
2023-11-23 00:59:03 +0900

    style: RankTag, LangTag 컴포넌트 생성

--- src/page/SearchStudy.jsx
+++ src/page/SearchStudy.jsx
@@ -17,22 +17,38 @@ const SelectOrderWay = () => {
   );
 };
 
+const RankTag = ({ children }) => {
+  return (
+    <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">
+      {children}
+    </span>
+  );
+};
+
+const LangTag = ({ children }) => {
+  return (
+    <span className="bg-gray-700 text-gray-300 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full border border-gray-500">
+      {children}
+    </span>
+  );
+};
+
 const MakeStudyCards = ({ title, description, tier, lang }) => {
   return (
     <a
       href="/"
-      className="block w-2/5 max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
+      className="block p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
     >
-      <div>
-        <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900">
+      <div className="mb-1 flex items-start justify-between">
+        <h5 className="text-lg font-bold tracking-tight text-gray-900">
           {title}
         </h5>
-        <h6 className="text-sm">{tier}</h6>
+        <RankTag>{tier}</RankTag>
       </div>
       <p className="font-normal text-gray-700 dark:text-gray-400">
         {description}
       </p>
-      <div>{lang}</div>
+      <LangTag>{lang}</LangTag>
     </a>
   );
 };
@@ -56,13 +72,13 @@ const SearchStudy = () => {
             <input
               type="search"
               id="default-search"
-              className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 focus:border-white rounded-lg bg-gray-50"
+              className="block w-full h-12 p-4 ps-10 text-sm text-gray-900 border border-gray-300 focus:border-white rounded-lg bg-gray-50"
               placeholder="스터디 이름을 입력해 주세요."
               required
             />
             <button
               type="submit"
-              className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2"
+              className="text-white absolute end-2.5 bottom-1.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2"
             >
               검색
             </button>
@@ -71,7 +87,7 @@ const SearchStudy = () => {
         <div className="grid place-items-end mt-2">
           <SelectOrderWay />
         </div>
-        <div className="mt-4 flex flex-wrap">
+        <div className="mt-4 grid grid-cols-2 gap-3">
           <MakeStudyCards
             title="test"
             description="testtesttest"
