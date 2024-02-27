a5f3899c9091aa231eb73c513e5d285e33ceefdc
coddingyun <igy2840@gmail.com>
2024-02-27 09:06:05 +0900

    fix: import React 추가

--- src/pages/MakeStudy/components/steps/WriteMeetingInfo.jsx
+++ src/pages/MakeStudy/components/steps/WriteMeetingInfo.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import RegionButton from '../../../../shared/components/RegionButton';
 import SelectComp from '../../../../shared/components/Select';
 import ModalLayout from '../ModalLayout';
