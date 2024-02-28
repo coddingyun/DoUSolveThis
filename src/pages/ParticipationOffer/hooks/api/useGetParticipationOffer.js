cfe70a853401ba1d09073b0fad7db401be6100b5
coddingyun <igy2840@gmail.com>
2024-02-28 16:15:28 +0900

    fix: await 추가

--- src/pages/ParticipationOffer/hooks/api/useGetParticipationOffer.js
+++ src/pages/ParticipationOffer/hooks/api/useGetParticipationOffer.js
@@ -3,7 +3,7 @@ import { useQuery } from "react-query";
 
 const useGetParticipationOffer = () => {
   return useQuery("participationOffer", async () => {
-    const response = api.get('/api/user/mypage/participation');
+    const response = await api.get('/api/user/mypage/participation');
     return response.data;
   })
 }
