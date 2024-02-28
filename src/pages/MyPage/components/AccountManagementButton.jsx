f2543ed0d443d09b46375f9deedbe7aa23633c9b
coddingyun <igy2840@gmail.com>
2024-02-28 13:53:29 +0900

    style: 버튼 cursor-ponter

--- src/pages/MyPage/components/AccountManagementButton.jsx
+++ src/pages/MyPage/components/AccountManagementButton.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import { Button } from "@chakra-ui/react";
 
 const AccountManagementButton = ({ title, onClick }) => {
   return (
-    <div
-      className="px-4 py-2.5 shadow-sm border border-solid border-gray-300 rounded-lg text-sm font-semibold"
+    <Button
+      className="px-4 py-2.5 shadow-sm border border-solid !bg-white border-gray-300 rounded-lg text-sm font-semibold"
       onClick={onClick}
     >
       {title}
-    </div>
+    </Button>
   );
 };
 
