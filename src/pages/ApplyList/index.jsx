6f8754810b096b89629e8bca67c96ba276106f14
coddingyun <igy2840@gmail.com>
2024-03-04 17:10:35 +0900

    feat: ApplyList 페이지 생성

---
+++ src/pages/ApplyList/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import TopNavigation from '../../shared/layout/TopNavigation'
+
+const ApplyList = () => {
+  return (
+    <TopNavigation>
+      <div className='px-8 py-10'>
+        <h1 className="text-gray-900 text-4xl font-bold mb-8">
+          신청 목록 보기
+        </h1>
+      </div>
+    </TopNavigation>
+  )
+}
+
+export default ApplyList
