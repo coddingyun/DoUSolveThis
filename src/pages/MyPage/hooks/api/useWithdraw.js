c32f115cd34e1dde1ccafad8626bebbc4514b3b2
coddingyun <igy2840@gmail.com>
2024-03-16 08:04:34 +0900

    refactor: useGetManagementStudy 확장성 고려하여 successCallback 파라미터화

--- src/pages/MyPage/hooks/api/useWithdraw.js
+++ src/pages/MyPage/hooks/api/useWithdraw.js
@@ -1,9 +1,19 @@
 import { useQuery } from 'react-query';
 import { api } from '../../../../shared/hooks/api';
 import useGetManagementStudy from '../../../../shared/hooks/api/useGetManagementStudy';
+import { useNavigate } from 'react-router-dom';
 
 const useWithdraw = () => {
-  const { refetch: refetchGetManagementStudy } = useGetManagementStudy();
+  const navigate = useNavigate();
+  const onSuccessCallback = data => {
+    if (data) {
+      // TODO. 스터디 탈퇴 시 스터디장 지정 페이지로 이동
+      // navigate('/')
+    } else {
+      navigate('/')
+    }
+  }
+  const { refetch: refetchGetManagementStudy } = useGetManagementStudy(onSuccessCallback);
 
   return useQuery('withdraw', async () => api.get('/api/withdraw'), {
     enabled: false,
--- src/shared/hooks/api/useGetManagementStudy.js
+++ src/shared/hooks/api/useGetManagementStudy.js
@@ -1,10 +1,7 @@
 import { useQuery } from 'react-query';
 import { api } from '.';
-import { useNavigate } from 'react-router-dom';
 
-const useGetManagementStudy = () => {
-  const navigate = useNavigate();
-
+const useGetManagementStudy = successCallback => {
   return useQuery(
     'managementStudy',
     async () => {
@@ -13,14 +10,7 @@ const useGetManagementStudy = () => {
     },
     {
       enabled: false,
-      onSuccess: data => {
-        if (data) {
-          // TODO. 스터디 탈퇴 시 스터디장 지정 페이지로 이동
-          // navigate('/')
-        } else {
-          navigate('/')
-        }
-      }
+      onSuccess: successCallback
     },
   );
 };
