523641f60da9ab9014238c7c71c3ee29fbec5c25
coddingyun <igy2840@gmail.com>
2024-03-16 08:23:19 +0900

    style: ManagerChange page 스타일링

--- src/pages/ManagerChange/components/StudyCardForManagerChange.jsx
+++ src/pages/ManagerChange/components/StudyCardForManagerChange.jsx
@@ -9,29 +9,39 @@ const StudyCardForManagerChange = ({ title, description }) => {
 
   const renderFooter = () => {
     if (isChanged) {
-      <div className="px-6 py-4 flex justify-between">
-        <h6>새로운 스터디장</h6>
-        <UserTag title={manager} tier={managerTier} />
-      </div>;
+      return (
+        <div className="px-6 py-4 flex justify-between">
+          <h6>새로운 스터디장</h6>
+          <UserTag title={manager} tier={managerTier} />
+        </div>
+      );
     } else {
-      <Button
-        className="!h-[36px] px-[14px] !text-brand-700 !bg-brand-50"
-        onClick={() => {
-          //TODO 다음 스터디장 지정 모달 열기
-        }}
-      >
-        스터디장 지정
-      </Button>;
+      return (
+        <div className='flex justify-end'>
+          <Button
+            className="!h-[36px] px-[14px] !text-brand-700 !bg-brand-50"
+            onClick={() => {
+              //TODO 다음 스터디장 지정 모달 열기
+            }}
+          >
+            스터디장 지정
+          </Button>
+        </div>
+      );
     }
   };
   return (
     <div className="block bg-white border border-gray-200 rounded-lg shadow">
-      <h5 className="p-6 text-3xl font-semibold tracking-tight text-gray-900">
-        {title}
-      </h5>
-      <h6>{description}</h6>
+      <div className="p-6">
+        <h5 className="text-3xl font-semibold tracking-tight text-gray-900 mb-2">
+          {title}
+        </h5>
+        <h6 className="text-sm text-gray-500">{description}</h6>
+      </div>
       <hr />
-      {renderFooter()}
+      <div className='px-6 py-4'>
+        {renderFooter()}
+      </div>
     </div>
   );
 };
--- src/pages/ManagerChange/index.jsx
+++ src/pages/ManagerChange/index.jsx
@@ -1,12 +1,32 @@
-
+import useGetManagementStudy from '../../shared/hooks/api/useGetManagementStudy';
+import StudyCardForManagerChange from './components/StudyCardForManagerChange';
 
 const ManagerChange = () => {
+  const { data } = useGetManagementStudy();
+
   return (
-    <div>
-      <h1>서비스 탈퇴</h1>
-      <h2>그동안 내가 관리했던 스터디의 새로운 스터디장을 지정해야 서비스 탈퇴가 가능합니다.</h2>
+    <div className='pt-[80px]'>
+      <div className='px-24'>
+        <div className='py-5 mb-8'>
+          <h1 className='font-semibold text-4xl text-gray-900 my-1'>서비스 탈퇴</h1>
+          <h2 className='text-base text-gray-500'>
+            그동안 내가 관리했던 스터디의 새로운 스터디장을 지정해야 서비스 탈퇴가
+            가능합니다.
+          </h2>
+        </div>
+        <div className='grid grid-cols-3 gap-6'>
+          {data &&
+            data.map((item, idx) => (
+              <StudyCardForManagerChange
+                key={`StudyCardForManagerChange#${idx}`}
+                title={item.title}
+                description={item.description}
+              />
+            ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ManagerChange;
