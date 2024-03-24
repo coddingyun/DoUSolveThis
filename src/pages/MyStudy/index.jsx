3d967ca34bd691f699d667585efff90a451aa6ea
coddingyun <igy2840@gmail.com>
2024-03-24 20:49:27 +0900

    feat: 내가 관리하는 스터디 라우팅

--- src/pages/MyStudy/index.jsx
+++ src/pages/MyStudy/index.jsx
@@ -3,13 +3,25 @@ import { Switch } from '@chakra-ui/react';
 import TopNavigation from '../../shared/layout/TopNavigation';
 import StudyCard from './components/Card';
 import useMyStudy from './hooks/api/useMyStudy';
+import { useSearchParams } from 'react-router-dom';
 
 const MyStudy = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const { myStudy, isLoading } = useMyStudy();
-  const [switchStatus, setSwitchStatus] = useState(false);
+  const [switchStatus, setSwitchStatus] = useState(
+    searchParams.get('managed') === 'true' ? true : false,
+  );
 
   const handleChangeSwitch = () => {
     setSwitchStatus(prev => !prev);
+    const prev = searchParams.get('managed');
+    if (prev === 'true') {
+      searchParams.set('managed', 'false');
+    } else {
+      searchParams.set('managed', 'true');
+    }
+
+    setSearchParams(searchParams);
   };
 
   return (
@@ -18,7 +30,7 @@ const MyStudy = () => {
         <div className="w-full flex flex-row-reverse items-center gap-3">
           <Switch
             colorScheme="purple"
-            isChecked={switchStatus}
+            isChecked={searchParams.get('managed') === 'true' ? true : false}
             onChange={handleChangeSwitch}
           />
           <h4 className="text-gray-700 text-[16px] font-semibold">
