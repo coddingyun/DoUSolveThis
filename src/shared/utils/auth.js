0266425f4ce9e1b72a35c31c35da41fee91dcd4c
coddingyun <igy2840@gmail.com>
2024-03-31 17:50:50 +0900

    feat: 로그아웃 시 토큰 제거

--- src/shared/utils/auth.js
+++ src/shared/utils/auth.js
@@ -1,9 +1,14 @@
-import { getCookie, setCookie } from './cookie';
+import { getCookie, removeCookie, setCookie } from './cookie';
 
 export const setAuthToken = (accessToken, refreshToken) => {
   setCookie('Access', accessToken);
   setCookie('Refresh', refreshToken);
 };
+
+export const removeAuthToken = () => {
+  removeCookie('Access');
+  removeCookie('Refresh');
+}
 
 export const getAccessToken = () => {
   return getCookie('Access');
