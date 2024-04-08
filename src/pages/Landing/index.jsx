578e20327282948a2ce63c88a190fc41c2dcbdb5
coddingyun <igy2840@gmail.com>
2024-04-08 20:56:10 +0900

    chore: logo svg 이름 변경

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { ReactComponent as Search } from '../../assets/search.svg';
 import { ReactComponent as Export } from '../../assets/export.svg';
-import { ReactComponent as LogoMark } from '../../assets/v-logo.svg';
+import { ReactComponent as LogoMark } from '../../assets/logo-v.svg';
 import LandingButton from './components/LandingButton';
 
 const Landing = () => {
