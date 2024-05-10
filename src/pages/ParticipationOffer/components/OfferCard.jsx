a04d071b93e24629d1df513924f2c5f605769fcf
coddingyun <igy2840@gmail.com>
2024-05-10 23:41:22 +0900

    feat: 거절하기 버튼 추가

--- src/pages/ParticipationOffer/components/OfferCard.jsx
+++ src/pages/ParticipationOffer/components/OfferCard.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import Line from '../../../shared/components/Line';
 import SimpleButton from '../../../shared/components/SimpleButton';
+import { Button } from '@chakra-ui/react';
 
-const OfferCard = ({ data, onClick }) => {
+const OfferCard = ({ data, onApproveClick, onRejectClick }) => {
   return (
     <div className="shadow-sm border border-solid border-gray-200 rounded-lg">
       <div className="px-6 py-4 flex justify-between items-center">
         <h2 className="text-gray-900 text-lg font-semibold">
           {data.studyTitle}
         </h2>
-        <SimpleButton title="수락하기" onClick={onClick} />
+        <div className='flex gap-2'>
+          <Button
+            className="!text-sm !rounded-lg border border-solid !border-gray-300 !bg-white text-gray-700 font-semibold mr-1"
+            variant="outlined"
+            onClick={onRejectClick}
+          >
+            거절하기
+          </Button>
+          <SimpleButton title="수락하기" onClick={onApproveClick} />
+        </div>
       </div>
       <Line />
       <div className="px-6 py-6 flex flex-col gap-4">
