d33932801f509f65da657415fd9ddfae55f51fd1
coddingyun <igy2840@gmail.com>
2024-05-11 11:44:06 +0900

    style: userId 넓이 조정

---
+++ src/shared/constants/typography.js
@@ -0,0 +1,46 @@
+export const typography = {
+  '.typography-2xl': {
+    fontSize: '72px',
+    lineHeight: '90px',
+  },
+  '.c-display-xl': {
+    fontSize: '60px',
+    lineHeight: '72px',
+  },
+  '.c-display-lg': {
+    fontSize: '48px',
+    lineHeight: '60px',
+  },
+  '.c-display-md': {
+    fontSize: '36px',
+    lineHeight: '44px',
+  },
+  '.c-display-sm': {
+    fontSize: '30px',
+    lineHeight: '38px',
+  },
+  '.c-display-xs': {
+    fontSize: '24px',
+    lineHeight: '38px',
+  },
+  '.c-text-xl': {
+    fontSize: '20px',
+    lineHeight: '30px',
+  },
+  '.c-text-lg': {
+    fontSize: '18px',
+    lineHeight: '28px',
+  },
+  '.c-text-md': {
+    fontSize: '16px',
+    lineHeight: '24px',
+  },
+  '.c-text-sm': {
+    fontSize: '14px',
+    lineHeight: '20px',
+  },
+  '.c-text-xs': {
+    fontSize: '12px',
+    lineHeight: '18px',
+  },
+};
