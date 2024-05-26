7ce8d0712ee98740f18a03b6d6dd849dc5c1b127
coddingyun <igy2840@gmail.com>
2024-05-26 13:41:25 +0900

    fix: levelToRank 랭크 명 수정

--- src/shared/constants/levelToRank.js
+++ src/shared/constants/levelToRank.js
@@ -1,31 +1,31 @@
 const levelToRank = [
   'Unrated',
-  'Bronze Ⅴ', // 1~5
+  'Bronze V', // 1~5
   'Bronze IV',
   'Bronze III',
   'Bronze II',
-  'Bronze Ⅰ',
-  'Silver Ⅴ', // 6~10
+  'Bronze I',
+  'Silver V', // 6~10
   'Silver IV',
   'Silver III',
   'Silver II',
   'Silver I',
-  'Gold Ⅴ', // 11~15
+  'Gold V', // 11~15
   'Gold IV',
   'Gold III',
   'Gold II',
   'Gold I',
-  'Platinum Ⅴ', // 16~ II0
+  'Platinum V', // 16~ II0
   'Platinum IV',
   'Platinum III',
   'Platinum II',
   'Platinum I',
-  'Diamond Ⅴ', //  II1~ II5
+  'Diamond V', //  II1~ II5
   'Diamond IV',
   'Diamond III',
   'Diamond II',
   'Diamond I',
-  'Ruby Ⅴ', //  II6~30
+  'Ruby V', //  II6~30
   'Ruby IV',
   'Ruby III',
   'Ruby II',
