f6f4ab2dad9822486dbb6570bbccab775d567ac1
coddingyun <igy2840@gmail.com>
2024-06-26 14:17:29 +0900

    feat: notice 삭제시 다시 userInfo 불러오기

--- src/shared/hooks/api/useDeleteNotices.js
+++ src/shared/hooks/api/useDeleteNotices.js
@@ -1,14 +1,15 @@
-import { useMutation, useQueryClient } from "react-query"
-import { api } from "."
+import { useMutation, useQueryClient } from 'react-query';
+import { api } from '.';
 
-const useDeleteNotices = (id) => {
+const useDeleteNotices = id => {
   const queryClient = useQueryClient();
 
   return useMutation(async () => api.delete(`/api/user/notices/${id}`), {
     onSuccess: () => {
       queryClient.invalidateQueries('notices');
-    }
-  })
-}
+      queryClient.invalidateQueries('userInfo', { refetchInactive: true });
+    },
+  });
+};
 
 export default useDeleteNotices;
