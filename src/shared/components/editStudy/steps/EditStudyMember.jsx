03050262f030d26e4e3341b83ce6001b66edd23f
coddingyun <igy2840@gmail.com>
2024-06-28 13:25:03 +0900

    feat: 스터디장 변경 반영

--- src/shared/components/editStudy/steps/EditStudyMember.jsx
+++ src/shared/components/editStudy/steps/EditStudyMember.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react';
 import InputContainer from '../../InputContainer';
 import Input from '../../Input';
-import { useEditStudyActions, useEditStudyMembers } from '../../../../store/studyStore';
+import {
+  useEditStudyActions,
+  useEditStudyManager,
+  useEditStudyMembers,
+} from '../../../../store/studyStore';
 import useCheckId from '../../../hooks/api/useCheckId';
 import { BaekjoonIdTag } from '../../Tag';
+import SelectComp from '../../Select';
 
 const EditStudyMember = () => {
   const [term, setTerm] = useState('');
   const [isValid, setIsValid] = useState(true);
   const members = useEditStudyMembers();
-  const { addMember } = useEditStudyActions();
+  const manager = useEditStudyManager();
+  const { addMember, setManager } = useEditStudyActions();
 
   const onCheckIdSuccessCallback = data => {
     if (data.valid) {
@@ -30,29 +36,42 @@ const EditStudyMember = () => {
   const borderStyle = !isValid ? '!border-error-300' : 'border-gray-300';
 
   return (
-    <InputContainer title="스터디원 추가(선택)">
-      <Input
-        placeholder="스터디원 사용자 ID 등록하기"
-        className={borderStyle}
-        value={term}
-        handleChangeValue={e => {
-          setTerm(e.target.value);
-          if (!isValid) {
-            setIsValid(true);
+    <div className='flex flex-col gap-4'>
+      <InputContainer title="스터디장 변경">
+        <SelectComp
+          value={manager.username}
+          handleChangeValue={e =>
+            setManager(members.find(m => m.username === e.target.value))
           }
-        }}
-        handleKeyDown={handleKeyDown}
-      />
-      {!isValid && (
+          options={members.map(m => m.username)}
+        />
+      </InputContainer>
+      <InputContainer title="스터디원 추가">
+        <Input
+          placeholder="스터디원 사용자 ID 등록하기"
+          className={borderStyle}
+          value={term}
+          handleChangeValue={e => {
+            setTerm(e.target.value);
+            if (!isValid) {
+              setIsValid(true);
+            }
+          }}
+          handleKeyDown={handleKeyDown}
+        />
+        {!isValid && (
           <span className="text-sm text-error-500">일치하는 ID가 없습니다</span>
         )}
-      <div className="flex flex-wrap gap-2">
-        {members &&
-          members.map((member, idx) => (
-            <BaekjoonIdTag key={`member#${idx}`} type="edit" member={member}>{member.username}</BaekjoonIdTag>
-          ))}
-      </div>
-    </InputContainer>
+        <div className="flex flex-wrap gap-2">
+          {members &&
+            members.map((member, idx) => (
+              <BaekjoonIdTag key={`member#${idx}`} type="edit" member={member}>
+                {member.username}
+              </BaekjoonIdTag>
+            ))}
+        </div>
+      </InputContainer>
+    </div>
   );
 };
 
